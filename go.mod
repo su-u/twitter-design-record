9c1eb01eed317f992958f0d445052b1097df630b
su-u <aaaiiiuuu35@gmail.com>
2021-12-04 23:00:43 +0900

    時間別に保存できるように

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github/su-u/twitter-design-record
 
 go 1.17
 
-require github.com/sclevine/agouti v3.0.0+incompatible // indirect
+require github.com/sclevine/agouti v3.0.0+incompatible
+
+require (
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.17.0 // indirect
+)
--- mian.go
+++ mian.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-  "fmt"
-
   "github.com/sclevine/agouti"
+  "log"
+  "os"
+  "time"
 )
 
+const layout = "2006-01-02 15-04-05"
+
 func main() {
+  urls := map[string]string{
+    "4毎の画像": "https://twitter.com/contain1101/status/1428169069363175430",
+    "FaceBook Appページ": "https://twitter.com/contain1101/status/1460774339511341056",
+  }
+
+  t := time.Now().Format(layout)
+  if err := os.MkdirAll("twitter/" + t, 0777); err != nil {
+    log.Fatal(err)
+  }
+
   // Chromeを利用することを宣言
   d := agouti.ChromeDriver()
   d.Start()
   defer d.Stop()
   p, _ := d.NewPage()
 
-  // 自動操作
-  p.Navigate("https://qiita.com/")
-  fmt.Println(
-    p.Title())
-  p.Screenshot("img/Screenshot01.png")
+  for k, v := range urls {
+    fName := "twitter/" + t + "/" + k + ".png"
 
-  p.FindByLink("もっと詳しく").Click()
-  fmt.Println(p.Title())
-  p.Screenshot("img/Screenshot02.png")
+    err := p.Navigate(v)
+    if err != nil {
+      log.Println("Failure: " + fName)
+      continue
+    }
+    time.Sleep(3 * time.Second)
+
+    p.Screenshot(fName)
+    log.Println("Done: " + fName)
+  }
 }
