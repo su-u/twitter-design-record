29ec8cdda4037868ca81659c50b01432179eb28b
su-u <aaaiiiuuu35@gmail.com>
2021-12-05 13:23:42 +0900

    Update mian.go

--- mian.go
+++ mian.go
@@ -11,7 +11,13 @@ const layout = "2006-01-02 15-04-05"
 
 func main() {
   urls := map[string]string{
+    "テキスト+Twitter Large Card": "https://twitter.com/TwitterDevJP/status/1354226852466835459",
+    "テキスト+Twitter Large Card（description）": "https://twitter.com/TwitterDevJP/status/971972766852251648",
+    "テキスト+Twitter Small Card": "https://twitter.com/TwitterDevJP/status/941468621880008704",
+    "テキスト+Twitter Small Card（プレビュー画像、description無し）": "https://twitter.com/TwitterDevJP/status/1300589123883773952",
+    "引用ツイート+画像": "https://twitter.com/TwitterDevJP/status/1172324315557023745",
     "4毎の画像": "https://twitter.com/contain1101/status/1428169069363175430",
+    "テキスト+URL+画像": "https://twitter.com/TwitterDevJP/status/732819781217325057",
     "FaceBook Appページ": "https://twitter.com/contain1101/status/1460774339511341056",
   }
 
